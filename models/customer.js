d4632c0874b69366e3d9fc019a107638eae594e3
mmiller-19 <matt_miller19@outlook.com>
2023-03-19 22:32:30 -0700

    begin creation of models

---
+++ models/customer.js
@@ -0,0 +1,10 @@
+const {Schema} = require('mongoose');
+
+const customerSchema = new Schema({
+    firstName: String,
+    lastName: String,
+    phoneNumber: Number
+});
+
+const Customer = model('Customer', customerSchema);
+module.exports = Customer;
