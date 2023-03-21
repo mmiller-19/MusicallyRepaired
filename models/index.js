b26fa76c8773f4469c954ed90dedd7ad70883321
mmiller-19 <matt_miller19@outlook.com>
2023-03-20 21:45:47 -0700

    add instrument to index.js

--- models/index.js
+++ models/index.js
@@ -3,5 +3,6 @@ const Message = require('./message');
 const Notes = require('./notes');
 const Repair = require('./repair');
 const Technician = require('./technician');
+const Instrument = require('./instrument');
 
-module.exports = { Customer, Message, Notes, Repair, Technician };
+module.exports = { Customer, Message, Notes, Repair, Technician, Instrument };
