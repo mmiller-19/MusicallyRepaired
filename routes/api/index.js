b53343c070f315b379d745d759b989c45f523872
VisualViolet <savannah_b_miller@outlook.com>
2023-03-22 16:24:11 -0700

    instrument routes completed

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const accountRoutes = require('./accountRoutes');
 const messageRoutes = require('./messageRoutes');
 const customerRoutes = require('./customerRoutes');
+const instrumentRoutes = require('./instrumentRoutes')
 
 router.use('/accounts', accountRoutes);
 router.use('/messages', messageRoutes);
 router.use('/customers', customerRoutes);
+router.use('/instruments', instrumentRoutes);
 
 module.exports = router;
