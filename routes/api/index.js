b373a323edee3967300b664f7ee0db9d796de53f
mmiller-19 <matt_miller19@outlook.com>
2023-03-22 16:29:10 -0700

    create repair shop routes for project

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const accountRoutes = require('./accountRoutes');
-const technicianRoutes =require('./technicianRoutes');
+const technicianRoutes = require('./technicianRoutes');
+const repairShopRoutes = require('./repairShopRoutes');
 
 router.use('/accounts', accountRoutes);
 router.use('/techs', technicianRoutes);
+router.use('/repairShops', repairShopRoutes);
 
 module.exports = router;
