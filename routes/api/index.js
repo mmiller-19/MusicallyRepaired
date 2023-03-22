5e1285d2b0c7a96b989ec0786916e8644e7079c6
VisualViolet <savannah_b_miller@outlook.com>
2023-03-22 16:33:18 -0700

    completed work order routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,10 +3,12 @@ const accountRoutes = require('./accountRoutes');
 const messageRoutes = require('./messageRoutes');
 const customerRoutes = require('./customerRoutes');
 const instrumentRoutes = require('./instrumentRoutes')
+const workOrderRoutes = require('./workOrderRoutes')
 
 router.use('/accounts', accountRoutes);
 router.use('/messages', messageRoutes);
 router.use('/customers', customerRoutes);
 router.use('/instruments', instrumentRoutes);
+router.use('/workorders', workOrderRoutes);
 
 module.exports = router;
