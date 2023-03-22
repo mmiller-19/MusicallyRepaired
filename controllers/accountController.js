2cd9b5811cedb8c2c6a3ffcdbdc96397a1793e28
mmiller-19 <matt_miller19@outlook.com>
2023-03-22 15:36:28 -0700

    more route stuff

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -6,5 +6,18 @@ module.exports = {
         Account.find()
             .then((accounts) => res.json(accounts))
                 .catch((error) => res.status(500).json(error));
+    },
+    getUserAccount(req, res) {
+        Account.findOne({_id : req.params.accountId})
+            .then((account) => 
+                !account
+                    ? res.status(404).json({message: 'No Account with that Id'})
+                    : res.json(account))
+                    .catch((error) => res.status(500).json(error));
+    },
+    createAccount(req, res) {
+        Account.create(req.body)
+            .then((newAccount) => res.json(newAccount))
+                .catch((error) => res.status(500).json(error));
     }
 };
