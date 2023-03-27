9591e8177053272188c6fad1cf305f9be64f53ad
VisualViolet <savannah_b_miller@outlook.com>
2023-03-27 14:47:43 -0700

    create sign up form with material ui

--- musically-repaired/src/App.js
+++ musically-repaired/src/App.js
@@ -2,6 +2,7 @@ import './assets/css/App.css';
 import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import Navigation from './components/Navigation';
 import Home from './components/Home';
+import SignUp from './components/SignUp';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navigation />
       <Routes>
         <Route path='/' element={<Home/>} />
+        <Route path='/signup' element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
