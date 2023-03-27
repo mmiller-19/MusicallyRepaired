1103e3ce6fdcfe4d8302f36827fd304afbcbf9a2
mmiller-19 <matt_miller19@outlook.com>
2023-03-27 15:17:54 -0700

    add login page to project

--- musically-repaired/src/App.js
+++ musically-repaired/src/App.js
@@ -2,6 +2,7 @@ import './assets/css/App.css';
 import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import Navigation from './components/Navigation';
 import Home from './components/Home';
+import Login from './components/Login'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navigation />
       <Routes>
         <Route path='/' element={<Home/>} />
+        <Route path='/login' element={<Login/>} />
       </Routes>
     </BrowserRouter>
   );
