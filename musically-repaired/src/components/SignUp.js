c59a4cd9f5ec68bb5bb6ceb7c43a575c9f5c904c
VisualViolet <savannah_b_miller@outlook.com>
2023-03-27 14:59:02 -0700

    wrap content in form tag and add link to login page

--- musically-repaired/src/components/SignUp.js
+++ musically-repaired/src/components/SignUp.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import FormControl from '@mui/material/FormControl';
 import Box from '@mui/material/Box';
 import { InputLabel, MenuItem, Select } from "@mui/material";
@@ -9,35 +10,38 @@ export default function SignUp() {
     return (
         <div className="SignUp">
             <h1>Create An Account</h1>
-            <Box
-                component="form"
-                sx={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    flexDirection: 'column',
-                    p: 1,
-                    m: 'auto',
-                    bgcolor: 'background.paper',
-                    borderRadius: 1,
-                    width: '30%',
-                    gap: '20px'
-                }}
-                noValidate
-                autoComplete="off"
-                >  
-                <TextField required id="email-text-field" label="Email" type="text" fullWidth/>
-                <TextField required id="password-text-field" label="Username" type="text" fullWidth/>
-                <FormControl fullWidth>
-                    <InputLabel id="account-type-select-label">Account Type</InputLabel>
-                    <Select labelId="account-type-select-label" label="Account Type" defaultValue='account'>
-                        <MenuItem value={"Customer"}>Customer</MenuItem>
-                        <MenuItem value={"Company"}>Company</MenuItem>
-                        <MenuItem value={"Technician"}>Technician</MenuItem>
-                        <MenuItem value={"Front Desk"}>Front Desk</MenuItem>
-                    </Select>
-                </FormControl>
-                <Button variant="contained" type="submit">Submit</Button>
-            </Box>
+            <form>
+                <Box
+                    component="form"
+                    sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        flexDirection: 'column',
+                        p: 1,
+                        m: 'auto',
+                        bgcolor: 'background.paper',
+                        borderRadius: 1,
+                        width: '30%',
+                        gap: '20px'
+                    }}
+                    noValidate
+                    autoComplete="off"
+                    >  
+                    <TextField required id="email-text-field" label="Email" type="text" fullWidth/>
+                    <TextField required id="password-text-field" label="Username" type="text" fullWidth/>
+                    <FormControl fullWidth>
+                        <InputLabel id="account-type-select-label">Account Type</InputLabel>
+                        <Select labelId="account-type-select-label" label="Account Type" defaultValue='account'>
+                            <MenuItem value={"Customer"}>Customer</MenuItem>
+                            <MenuItem value={"Company"}>Company</MenuItem>
+                            <MenuItem value={"Technician"}>Technician</MenuItem>
+                            <MenuItem value={"Front Desk"}>Front Desk</MenuItem>
+                        </Select>
+                    </FormControl>
+                    <Button variant="contained" type="submit">Submit</Button>
+                </Box>
+            </form>
+            <small>Have an account? <Link to="/login">Log in here</Link></small>
         </div>
     )
 }
